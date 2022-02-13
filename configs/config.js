0daeb5de4b663d892888a85df8724419131a4c05
imri.barr <imri.barr@walkme.com>
2022-02-13 14:55:38 +0200

    build with async await

--- configs/config.js
+++ configs/config.js
@@ -12,7 +12,14 @@ const loose = true
 const babelSetup = {
   babelrc: false,
   presets: [['@babel/preset-env', { modules: false, loose }]],
-  plugins: [['@babel/plugin-proposal-class-properties', { loose }]],
+  runtimeHelpers: true,
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose }],
+    ["@babel/plugin-transform-runtime", {
+        "regenerator": true
+      }
+    ]
+  ],
   exclude: 'node_modules/**',
 }
 
